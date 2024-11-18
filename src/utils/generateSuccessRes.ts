e43980b08fa8517313fc99032616f6fdbdb8bd67
zeroqs <smthq@bk.ru>
2024-11-18 14:51:22 +0300

    chore: delete utils

--- src/utils/generateSuccessRes.ts
+++
@@ -1,8 +0,0 @@
-import { ApiSuccessResponseDto } from './dto/response.dto';
-
-export const generateSuccessResponse = (
-  message: string = 'Success',
-): ApiSuccessResponseDto => ({
-  statusCode: 200,
-  message,
-});
